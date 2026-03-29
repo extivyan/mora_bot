37d0fe602f2f428471f216b077517a3dc8b5ac21
extivyan <nagyomar340@gmail.com>
2026-03-29 05:46:08 -0400

    fix crash add express server

--- index.js
+++ index.js
@@ -153,3 +153,16 @@ process.on('SIGTERM', () => {
 
 console.clear();
 handleConnection();
+
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Bot is running 🚀");
+});
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
